5881e00f091ae9519136820ab77a6197230b2441
Christopher <schulze.christopher@icloud.com>
2025-07-05 22:17:20 +0200

    Extend cache benchmark and update results

--- cache/benches/cache_bench.rs
+++ cache/benches/cache_bench.rs
@@ -1,6 +1,6 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 use cache::CacheManager;
-use api_client::{MediaItem, MediaMetadata, VideoMetadata};
+use api_client::{MediaItem, MediaMetadata, VideoMetadata, Album};
 use tempfile::NamedTempFile;
 
 fn sample_media_item(id: &str) -> MediaItem {
@@ -17,6 +17,35 @@ fn sample_media_item(id: &str) -> MediaItem {
             video: None,
         },
         filename: format!("{}.jpg", id),
+    }
+}
+
+fn sample_media_item_with_mime(id: &str, mime: &str) -> MediaItem {
+    MediaItem {
+        id: id.to_string(),
+        description: None,
+        product_url: "http://example.com".into(),
+        base_url: "http://example.com/base".into(),
+        mime_type: mime.into(),
+        media_metadata: MediaMetadata {
+            creation_time: "2023-01-01T00:00:00Z".into(),
+            width: "1".into(),
+            height: "1".into(),
+            video: None,
+        },
+        filename: format!("{}.jpg", id),
+    }
+}
+
+fn sample_album(id: &str) -> Album {
+    Album {
+        id: id.to_string(),
+        title: Some("Album".into()),
+        product_url: None,
+        is_writeable: None,
+        media_items_count: None,
+        cover_photo_base_url: None,
+        cover_photo_media_item_id: None,
     }
 }
 
@@ -70,6 +99,55 @@ fn bench_load_all_10k(c: &mut Criterion) {
     });
 }
 
+fn bench_load_all_100k(c: &mut Criterion) {
+    let tmp = NamedTempFile::new().unwrap();
+    let cache = CacheManager::new(tmp.path()).unwrap();
+    for i in 0..100_000u32 {
+        let item = sample_media_item(&i.to_string());
+        cache.insert_media_item(&item).unwrap();
+    }
+    c.bench_function("load_all_100k", |b| {
+        b.iter(|| {
+            let _ = cache.get_all_media_items().unwrap();
+        })
+    });
+}
+
+fn bench_mime_type_query(c: &mut Criterion) {
+    let tmp = NamedTempFile::new().unwrap();
+    let cache = CacheManager::new(tmp.path()).unwrap();
+    for i in 0..10_000u32 {
+        let mime = if i % 2 == 0 { "image/jpeg" } else { "video/mp4" };
+        let item = sample_media_item_with_mime(&i.to_string(), mime);
+        cache.insert_media_item(&item).unwrap();
+    }
+    c.bench_function("mime_type_query", |b| {
+        b.iter(|| {
+            let _ = cache.get_media_items_by_mime_type("image/jpeg").unwrap();
+        })
+    });
+}
+
+fn bench_album_query(c: &mut Criterion) {
+    let tmp = NamedTempFile::new().unwrap();
+    let cache = CacheManager::new(tmp.path()).unwrap();
+    for a in 0..10u32 {
+        let album = sample_album(&format!("album{}", a));
+        cache.insert_album(&album).unwrap();
+    }
+    for i in 0..10_000u32 {
+        let album_id = format!("album{}", i % 10);
+        let item = sample_media_item(&i.to_string());
+        cache.insert_media_item(&item).unwrap();
+        cache.associate_media_item_with_album(&item.id, &album_id).unwrap();
+    }
+    c.bench_function("album_query", |b| {
+        b.iter(|| {
+            let _ = cache.get_media_items_by_album("album0").unwrap();
+        })
+    });
+}
+
 fn bench_camera_model_query(c: &mut Criterion) {
     let tmp = NamedTempFile::new().unwrap();
     let cache = CacheManager::new(tmp.path()).unwrap();
@@ -85,6 +163,14 @@ fn bench_camera_model_query(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, bench_load_all, bench_load_all_10k, bench_camera_model_query);
+criterion_group!(
+    benches,
+    bench_load_all,
+    bench_load_all_10k,
+    bench_load_all_100k,
+    bench_camera_model_query,
+    bench_mime_type_query,
+    bench_album_query
+);
 criterion_main!(benches);
 
